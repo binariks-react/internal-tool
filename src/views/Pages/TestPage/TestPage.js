78c238ca8757ae65777e8767bdf7564fd8f8fbdb
Rostyk Vishko <rvishko@binariks.com>
2019-04-04 15:58:54 +0300

    added progress bar wrapper

--- src/views/Pages/TestPage/TestPage.js
+++ src/views/Pages/TestPage/TestPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Button from 'views/Components/Controls/Button';
+import ProgressBar from '../../Components/Controls/ProgressBar';
 
 const Wrapper = styled.div`
   button + button {
@@ -12,8 +13,8 @@ const Wrapper = styled.div`
 `;
 
 const TestPage = () => (
-  <div>
-    <Wrapper>
+  <Wrapper>
+    <div>
       <Button color="primary">Hello</Button>
       <Button color="secondary">Hello</Button>
       <Button color="success">Hello</Button>
@@ -25,8 +26,8 @@ const TestPage = () => (
       <Button color="royalBlue">Hello</Button>
       <Button color="java">Hello</Button>
       <Button color="salmon">Hello</Button>
-    </Wrapper>
-    <Wrapper>
+    </div>
+    <div>
       <Button fill="outline" color="primary">Hello</Button>
       <Button fill="outline" color="secondary">Hello</Button>
       <Button fill="outline" color="success">Hello</Button>
@@ -38,8 +39,8 @@ const TestPage = () => (
       <Button fill="outline" color="royalBlue">Hello</Button>
       <Button fill="outline" color="java">Hello</Button>
       <Button fill="outline" color="salmon">Hello</Button>
-    </Wrapper>
-    <Wrapper>
+    </div>
+    <div>
       <Button small color="primary">Hello</Button>
       <Button small color="secondary">Hello</Button>
       <Button small color="success">Hello</Button>
@@ -51,8 +52,8 @@ const TestPage = () => (
       <Button small color="royalBlue">Hello</Button>
       <Button small color="java">Hello</Button>
       <Button small color="salmon">Hello</Button>
-    </Wrapper>
-    <Wrapper>
+    </div>
+    <div>
       <Button fill="outline" small color="primary">Hello</Button>
       <Button fill="outline" small color="secondary">Hello</Button>
       <Button fill="outline" small color="success">Hello</Button>
@@ -64,8 +65,17 @@ const TestPage = () => (
       <Button fill="outline" small color="royalBlue">Hello</Button>
       <Button fill="outline" small color="java">Hello</Button>
       <Button fill="outline" small color="salmon">Hello</Button>
-    </Wrapper>
-  </div>
+    </div>
+    <div>
+      <ProgressBar color="primary" min={-200} max={200} value={-125} />
+      <ProgressBar color="secondary" min={0} max={50} value={25} />
+      <ProgressBar color="success" min={50} max={150} value={125} />
+      <ProgressBar color="danger" min={5} max={25} value={30} />
+      <ProgressBar color="warning" min={5} max={25} value={4} />
+      <ProgressBar color="info" min={250} max={150} value={175} />
+    </div>
+  </Wrapper>
 );
 
 export default TestPage;
+
