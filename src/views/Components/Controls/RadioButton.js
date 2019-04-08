f945f6f5c56237a258661a61bdf0ef4cd4758887
Ihor Samilo <Ihor33804@ukr.net>
2019-04-08 17:53:26 +0300

    refractoring code

--- src/views/Components/Controls/RadioButton.js
+++ src/views/Components/Controls/RadioButton.js
@@ -20,47 +20,48 @@ const RadioButtonInput = styled.input`
 const RadioButtonIcon = styled.span`
   display: inline-block;
   box-sizing: border-box;
-  color: #818994;
+  color: ${props => props.theme.colors.secondary};
   width: 1.125rem;
   border-radius: 50%;
   height: 1.125rem;
-  background-color: #fff;
-  border: 1px solid #e1e5eb;
+  background-color: ${props => props.theme.colors.white};
+  border: 1px solid ${props => props.theme.colors.border};
   transition: all 250ms cubic-bezier(0.27, 0.01, 0.38, 1.06);
   box-shadow: none;
   input:checked + & {
-    border: 5px solid #007bff;
+    border: 5px solid ${props => props.theme.colors.primary};
   }
 
-  input[type='radio'][|checked][disabled] + & {
-    background-color: #818994;
-    border: 5px solid #e9ecef;
+  input[type='radio'][checked][disabled] + & {
+    background-color: ${props => props.theme.colors.secondary};
+    border: 5px solid ${props => props.theme.colors.lightGrey};
   }
 
   input:disabled + & {
-    background-color: #e9ecef;
+    background-color: ${props => props.theme.colors.lightGrey};
   }
 `;
 
 const RadioButtonSpan = styled.span`
   margin-left: 5px;
-  color: #5a6169;
+  color: ${props => props.theme.colors.secondary};
 
-  input:disabled ~ &{
-    color: #818994;
+  input:disabled ~ & {
+    opacity: 0.7;
+    color: ${props => props.theme.colors.secondary};
   }
 `;
 
-const RadioButton =	({ defaultChecked, disabled, title }) => (
-  <RadioButtonWrapper {...{ disabled }}>
+const RadioButton = ({ defaultChecked, disabled, title, ...props }) => (
+  <RadioButtonWrapper disabled={disabled} {...props}>
     <RadioButtonInput
       type="radio"
-      {...{ defaultChecked, disabled }}
+      defaultChecked={defaultChecked}
+      disabled={disabled}
+      {...props}
     />
-    <RadioButtonIcon />
-    <RadioButtonSpan>
-      {title || 'radio'}
-    </RadioButtonSpan>
+    <RadioButtonIcon {...props} />
+    <RadioButtonSpan {...props}>title</RadioButtonSpan>
   </RadioButtonWrapper>
 );
 
